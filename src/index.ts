7147a191e3a19be9720271991d86ac26c70dc826
darren-rose <darrenwrose@gmail.com>
2020-06-19 21:56:51 +0200

    move types to models

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import {Context, Handler} from 'aws-lambda';
 
-interface Request {
-    key1: string,
-    key2: string,
-    key3: string
-}
-
-interface Response {
-    message: string
-}
+import {Request, Response} from './models';
 
 export const handler : Handler<Request, Response> = async (event: Request, context: Context) : Promise<Response> => {
     console.log('awsRequestId', context.awsRequestId);
---
+++ src/models.ts
@@ -0,0 +1,9 @@
+export interface Request {
+    key1: string,
+    key2: string,
+    key3: string
+}
+
+export interface Response {
+    message: string
+}
